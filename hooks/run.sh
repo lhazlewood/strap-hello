6908e33631ed4d3dc13e26a95ef387d7fc9edca0
Les Hazlewood <121180+lhazlewood@users.noreply.github.com>
2018-07-02 11:51:18 -0400

    minor local file path cleanup

--- hooks/run.sh
+++ hooks/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-strap::lib::import logging || source ././../../../ultimatedotfiles/strap/lib/logging.sh
+strap::lib::import logging || source ../../../ultimatedotfiles/strap/lib/logging.sh
 
 strap::running "Saying hello"
 strap::action "Hello!"

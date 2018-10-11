f7fec259bc20fe375538f055f87df86028a22737
Les Hazlewood <121180+lhazlewood@users.noreply.github.com>
2018-10-11 18:29:24 -0400

    testing bash functions and subshells on macos 10.12.x

--- hooks/run.sh
+++ hooks/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+PACKAGE_DIR="$(dirname "$(strap:fs:path "${BASH_SOURCE[0]}")")"
+
 strap::lib::import logging || source ../../../ultimatedotfiles/strap/lib/logging.sh
 
 strap::bot "Hello"
 
 strap::running "Saying hello"
-strap::action "Hello World!"
+strap::action "Hello World! PACKAGE_DIR=$PACKAGE_DIR"
 strap::ok
